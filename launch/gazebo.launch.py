5a1171c268ce7e14b2d88a3fb3c825ba5ae89ac2
jota975x <joaquinrgarciar@hotmail.com>
2024-11-01 14:45:14 -0400

    Added environment, robot model

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -1,9 +1,11 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 from launch_ros.actions import Node
 
 
@@ -12,15 +14,50 @@ def generate_launch_description():
     # Include the robot_state_publisher launch file
     package_name='navigationrl'
 
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name),'launch','rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+    
+    # Define World
+    world_file_name = 'env_world.world'
+    world_path = os.path.join(
+        get_package_share_directory(package_name), 'worlds', world_file_name
+    )
+
     # Include the Gazebo launch file
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'),'launch','gazebo.launch.py'
-        )])
+        )]),
+        launch_arguments={'world': world_path}.items()
+    )
+
+    # Run the bot spawner
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
+    
+    
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
     )
     
     # Launch all!
     return LaunchDescription([
-
-        gazebo
+        rsp,
+        gazebo,
+        spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
